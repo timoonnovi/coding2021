fec4ceba6760a9ed2ee1d58d99e6bf480f1f34f6
timoonnovi <pasko.id@phystech.edu>
2021-09-30 23:39:02 +0300

    Add files via upload

---
+++ DZ-4/4-1/4-1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include "Vector.h"
+
+int main()
+{
+	Vector v;
+
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Введите вектор v:\n";
+	std::cin >> v;
+	std::cout << '\n';
+	std::cout << "Модуль вектора v:\n";
+	std::cout << v.abs() << '\n';
+	std::cout << "Обращённый вектор v:\n";
+	v = -v;
+	std::cout << -v;
+
+	int a;
+
+	std::cout << "Введите число:\n";
+	std::cin >> a;
+	std::cout << "v * a = " << v * a << '\n';
+	std::cout << "Введите второй вектор u:\n";
+	
+	Vector u;
+
+	std::cin >> u;
+	std::cout << "v + u =\n" << v + u << '\n';
+	std::cout << "v - u =\n" << v - u << '\n';
+	std::cout << "Скалярное произведение (v, u) = " << scalar(v, u) << '\n';
+
+	return 0;
+}
---
+++ DZ-4/4-1/Vector.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include <vector>
+#include <iostream>
+
+// Это вектор из линейной алгебры. 
+// Имеет 2 поля – собственно компоненты вектора и размерность пространства.
+struct Vector
+{
+	std::vector<double> components;
+	unsigned int dim;
+
+	// Функция расчёта модуля вектора.
+	double abs();
+};
+
+// Вывод вектора. 
+// Сначала размерность, затем компоненты.
+std::ostream& operator<<(std::ostream& ostr,
+	const Vector& vec);
+
+// Ввод вектора.
+// Сначала размерность, затем компоненты.
+std::istream& operator>>(std::istream& istr, Vector& v);
+
+// Умножение вектора на число.
+Vector operator*(const Vector& v, const double& num);
+
+// Умножение вектора на число.
+Vector& operator*=(Vector& v, const double& num);
+
+// Умножение числа на вектор.
+Vector operator*(const double& num, const Vector& v);
+
+// Сложение векторов.
+Vector operator+(const Vector& v1, const Vector& v2);
+
+// Тоже сложение векторов.
+Vector& operator+=(Vector& v1, const Vector& v2);
+
+// Вычитание векторов.
+Vector operator-(const Vector& v1, const Vector& v2);
+
+// Тоже вычитание векторов.
+Vector& operator-=(Vector& v1, const Vector& v2);
+
+// Обращение вектора. В язычество. Во славу Сварога.
+Vector& operator-(Vector& v);
+
+// Функция скалярного произведения векторов.
+double scalar(const Vector& v1, const Vector& v2);
