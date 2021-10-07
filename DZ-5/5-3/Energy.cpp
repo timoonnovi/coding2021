b31b43e471688a24a343c56af0fd98db192e60ba
timoonnovi <pasko.id@phystech.edu>
2021-10-08 00:27:57 +0300

    Add files via upload

---
+++ DZ-5/5-3/Energy.cpp
@@ -0,0 +1,83 @@
+﻿#include "Energy.h"
+#include <map>
+#include <string>
+
+Energy::Energy(double value, Units unit)
+{
+	m_value = new double(value);
+	m_unit = new Units(unit);
+	isMemAllocated = true;
+	return;
+}
+
+Energy::~Energy()
+{
+	if (isMemAllocated)
+	{
+		delete m_unit;
+		m_unit = nullptr;
+		delete m_value;
+		m_value = nullptr;
+	}
+	return;
+}
+
+void Energy::convert(Units unit)
+{
+	if (*m_unit == unit) return;
+
+	double tmp1 = Constants[unit]; 
+	double tmp2 = Constants[*m_unit];
+	double tmp = tmp1 / tmp2;
+
+	*m_value *= tmp;
+
+	*m_unit = unit;
+}
+
+std::ostream& operator<<(std::ostream& ostr, const Units& unit)
+{
+	std::string tmp;
+
+	if (unit == Units::J) tmp = "J";
+	if (unit == Units::CAL) tmp = "CAL";
+	if (unit == Units::ERG) tmp = "ERG";
+	if (unit == Units::eV) tmp = "eV";
+
+	ostr << tmp;
+
+	return ostr;
+}
+
+std::istream& operator>>(std::istream& istr, Units& unit)
+{
+	std::string tmp;
+	istr >> tmp;
+
+	if (tmp == "J") unit = Units::J;
+	else if (tmp == "CAL") unit = Units::CAL;
+	else if (tmp == "ERG") unit = Units::ERG;
+	else if (tmp == "eV") unit = Units::eV;
+
+	return istr;
+}
+
+std::ostream& operator<<(std::ostream& ostr, const Energy& en)
+{
+	ostr << *en.m_value << ' ' << *en.m_unit << '\n';
+	return ostr;
+}
+
+std::istream& operator>>(std::istream& istr, Energy& en)
+{
+	double val;
+	Units unit;
+
+	istr >> val >> unit;
+
+	en.m_value = new double(val);
+	en.m_unit = new Units(unit);
+	en.isMemAllocated = true;
+
+	return istr;
+}
---
+++ DZ-5/5-3/Energy.h
@@ -0,0 +1,50 @@
+﻿#ifndef ENERGY_H
+#define ENERGY_H
+
+#include <iostream>
+#include <map>
+#include <cmath>
+
+enum class Units
+{
+	J,
+	ERG,
+	CAL,
+	eV
+};
+
+std::ostream& operator<<(std::ostream&, const Units&);
+std::istream& operator>>(std::istream&, Units&);
+
+static std::map <Units, double> Constants = { {Units::J, 1}, {Units::ERG, pow(10, 7)}, 
+	{Units::CAL, 0.24}, {Units::eV, 6.24 * pow(10, 18)} };
+
+class Energy
+{
+private:
+	bool isMemAllocated = false;
+	double* m_value;
+	Units* m_unit;
+	
+public:
+	Energy() : m_value(nullptr), m_unit(nullptr) {}
+	Energy(double, Units);
+	Energy(const Energy& another) : m_value (another.m_value), m_unit (another.m_unit) {}
+	~Energy();
+
+	void convert(Units unit);
+
+	// Геттеры
+	double getValue() { return *m_value; }
+	Units getUnit() { return *m_unit; }
+
+	// Сеттеры
+	void setValue(double tmp) { *m_value = tmp; }
+	void setUnit(Units tmp) { *m_unit = tmp; }
+
+	// Ввод-вывод
+	friend std::ostream& operator<<(std::ostream&, const Energy&);
+	friend std::istream& operator>>(std::istream&, Energy&);
+};
+
+#endif // ENERGY_H
