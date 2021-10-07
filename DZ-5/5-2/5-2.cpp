cf59d68f1e2fb4554dec90609298358e89b50c61
timoonnovi <pasko.id@phystech.edu>
2021-10-07 20:00:22 +0300

    Add files via upload

---
+++ DZ-5/5-2/5-2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include "Vector.h"
+
+int main()
+{
+	Vector v;
+
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Введите вектор v (размерность и компоненты):\n";
+	std::cin >> v;
+	std::cout << '\n';
+	std::cout << "Модуль вектора v: ";
+	std::cout << v.abs() << "\n\n";
+	std::cout << "Обращённый вектор v:\n";
+	std::cout << -v << '\n';
+
+	int a;
+
+	std::cout << "Введите число:\n";
+	std::cin >> a;
+	std::cout << "\nv * a =\n" << v * a << '\n';
+	std::cout << "Введите второй вектор u:\n";
+
+	Vector u;
+
+	std::cin >> u;
+	std::cout << "v + u =\n" << v + u << '\n';
+	std::cout << "v - u =\n" << v - u << '\n';
+	std::cout << "Скалярное произведение (v, u) = " << scalar(v, u) << '\n';
+
+	return 0;
+}
---
+++ DZ-5/5-2/Vector.h
@@ -0,0 +1,56 @@
+﻿#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <vector>
+#include <iostream>
+
+// Это вектор из линейной алгебры. 
+// Имеет 2 поля – собственно компоненты вектора и размерность пространства.
+class Vector
+{
+private:
+	std::vector<double> components;
+	unsigned int dim;
+
+public:
+	// Функция расчёта модуля вектора.
+	double abs();
+
+	// Вывод вектора. 
+	// Сначала размерность, затем компоненты.
+	friend std::ostream& operator<<(std::ostream& ostr,
+		const Vector& vec);
+
+	// Ввод вектора.
+	// Сначала размерность, затем компоненты.
+	friend std::istream& operator>>(std::istream& istr, Vector& v);
+
+	// Умножение вектора на число.
+	friend Vector operator*(const Vector& v, const double& num);
+
+	// Умножение вектора на число.
+	friend Vector& operator*=(Vector& v, const double& num);
+
+	// Умножение числа на вектор.
+	friend Vector operator*(const double& num, const Vector& v);
+
+	// Сложение векторов.
+	friend Vector operator+(const Vector& v1, const Vector& v2);
+
+	// Тоже сложение векторов.
+	friend Vector& operator+=(Vector& v1, const Vector& v2);
+
+	// Вычитание векторов.
+	friend Vector operator-(const Vector& v1, const Vector& v2);
+
+	// Тоже вычитание векторов.
+	friend Vector& operator-=(Vector& v1, const Vector& v2);
+
+	// Обращение вектора. В язычество. Во славу Сварога.
+	friend Vector& operator-(Vector& v);
+
+	// Функция скалярного произведения векторов.
+	friend double scalar(const Vector& v1, const Vector& v2);
+};
+
+#endif // VECTOR_H
