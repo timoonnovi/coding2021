8fd78798be5846d7829a3d809213d28e67b6982b
timoonnovi <pasko.id@phystech.edu>
2021-10-14 20:25:52 +0300

    Add files via upload

---
+++ DZ-6/6-1/SmartPtr.h
@@ -0,0 +1,26 @@
+﻿#ifndef SMART_PTR
+#define SMART_PTR
+
+#include <iostream>
+#include <utility>
+
+class SmartPtr
+{
+private:
+	double* m_ptr;
+	bool isMemAllocated = false;
+public:
+	// Конструкторы
+	SmartPtr() = default;
+	SmartPtr(double* ptr) : m_ptr(ptr) {}
+	SmartPtr(double);
+	SmartPtr(const SmartPtr& another) : m_ptr(std::move(another.m_ptr)) {}
+
+	// Деструктор
+	~SmartPtr();
+
+	friend double& operator* (SmartPtr);
+	friend std::ostream& operator<<(std::ostream&, const SmartPtr&);
+};
+
+#endif // SMART_PTR
