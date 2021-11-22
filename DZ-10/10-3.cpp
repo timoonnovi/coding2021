c19f38d5196a0d8c799b81e2db3858291cfa5666
timoonnovi <pasko.id@phystech.edu>
2021-11-22 15:35:06 +0300

    Update 10-3.cpp

--- DZ-10/10-3.cpp
+++ DZ-10/10-3.cpp
@@ -3,24 +3,26 @@
 
 #include <iostream>
 
-int size_in_bytes_1 = 0;
-
-template<typename T>
-void Size1(T arg)
+template<typename RT, typename ... Types>
+RT Size1(RT firstArg, Types... args)
 {
-    size_in_bytes_1 += sizeof(arg);
-    return;
+    if constexpr (sizeof...(args) > 0)
+    {
+        return Size1(firstArg) + Size1(args...);
+    }
+    else
+    {
+        return sizeof(firstArg);
+    }
 }
 
-template<typename T, typename ... Types>
-void Size1(T firstArg, Types... args)
-{
-    Size1(firstArg);
-    Size1(args...);
-    return;
+template<typename T, typename... Types>
+void print_second_realization(T first_argument, Types... arguments) {
+    std::cout << first_argument << ' ';
+    if constexpr (sizeof...(arguments) > 0) {
+        print_second_realization(arguments...);
+    }
 }
-
-int size_in_bytes_2 = 0;
 
 template<typename ... Types>
 auto Size2(Types... args)
@@ -31,10 +33,14 @@ auto Size2(Types... args)
 int main()
 {
     int a = 1, b = 2, c = 5;
-    Size1<int, int, int>(a, b, c);
+    int size_in_bytes_1, size_in_bytes_2;
+
+    size_in_bytes_1 = Size1<int, int, int>(a, b, c);
     size_in_bytes_2 = Size2<int, int, int>(a, b, c);
+
     std::cout << "First method:  " << size_in_bytes_1 << " bytes." << '\n';
     std::cout << "Second method: " << size_in_bytes_2 << " bytes." << '\n';
+
     return 0;
 }
 
